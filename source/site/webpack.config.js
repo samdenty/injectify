51c15f415d21fcde7894e3a4fe8e40a33ae1be53
samdd <sam@samdd.me>
2017-11-08 23:59:44 +0000

    [+] Uglify JS

--- source/site/webpack.config.js
+++ source/site/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 	entry: ["./main.jsx"],
@@ -18,10 +19,7 @@ module.exports = {
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
 		}),
-		new webpack.optimize.UglifyJsPlugin({
-		  include: /\.min\.js$/,
-		  minimize: true
-		})
+		new UglifyJSPlugin()
 	],
 	resolve: {
 		extensions: ['*', '.js', '.jsx']
