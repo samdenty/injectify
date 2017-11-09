7e71e4cecad35b58b32b9ca2e0934414c5c062dd
samdd <sam@samdd.me>
2017-11-09 19:31:51 +0000

    [*] socket-io fix

--- source/site/main.jsx
+++ source/site/main.jsx
@@ -3,12 +3,14 @@ import React, { Component } from "react"
 import queryString from "query-string"
 import io from "socket.io-client"
 import url from "url"
+
 const development = process.env.NODE_ENV == 'development' ? true : false
+let socket
 if (development) {
-	const socket = io('http://localhost:2053')
- } else {
-	const socket = io('https://injectify.samdd.me:2053')
- }
+	socket = io('http://localhost:2053')
+} else {
+	socket = io('https://injectify.samdd.me:2053')
+}
 
 console.log("%c  _____        _           _   _  __       \n  \\_   \\_ __  (_) ___  ___| |_(_)/ _|_   _ \n   / /\\/ '_ \\ | |/ _ \\/ __| __| | |_| | | |\n/\\/ /_ | | | || |  __/ (__| |_| |  _| |_| |\n\\____/ |_| |_|/ |\\___|\\___|\\__|_|_|  \\__, |\n            |__/  " + "%chttps://samdd.me" + "%c   |___/ " + "\n", "color: #ef5350; font-weight: bold", "color: #FF9800", "color: #ef5350", {
 	sha: git.last_commit.long_sha,
