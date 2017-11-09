0ca2a560e09fe5d2fb70cd3aacff34045fcfb703
samdd <sam@samdd.me>
2017-11-09 00:34:29 +0000

    [*] Auth fixes

--- source/site/main.jsx
+++ source/site/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM, { render } from 'react-dom'
 import React, { Component } from "react"
 import queryString from "query-string"
 import io from "socket.io-client"
+import url from "url"
 
 console.log("%c  _____        _           _   _  __       \n  \\_   \\_ __  (_) ___  ___| |_(_)/ _|_   _ \n   / /\\/ '_ \\ | |/ _ \\/ __| __| | |_| | | |\n/\\/ /_ | | | || |  __/ (__| |_| |  _| |_| |\n\\____/ |_| |_|/ |\\___|\\___|\\__|_|_|  \\__, |\n            |__/  " + "%chttps://samdd.me" + "%c   |___/ " + "\n", "color: #ef5350; font-weight: bold", "color: #FF9800", "color: #ef5350", {
 	sha: git.last_commit.long_sha,
@@ -28,16 +29,14 @@ class LoginWithGithub extends Component {
 		)
 	}
 	auth() {
-		window.open(`https://github.com/login/oauth/authorize?client_id=95dfa766d1ceda2d163d${process.env.NODE_ENV == 'development' ? `&state=localhost` : ``}`, "popup", "height=600,width=500")
-	}
-
-	componentDidMount() {
+		global.oauth = window.open(`https://github.com/login/oauth/authorize?client_id=95dfa766d1ceda2d163d${process.env.NODE_ENV == 'development' ? `&state=localhost` : ``}`, "popup", "height=600,width=500")
 		window.addEventListener("message", data => {
 			if (data.origin == "http://localhost:3000" || data.origin == "https://injectify.samdd.me") {
 				if (typeof oauth !== "undefined") {
 					oauth.close()
 					const parsed = url.parse(data.data)
 					const urlData = queryString.parse(parsed.hash)
+					console.log(parsed)
 
 					if (urlData.access_token) {
 						localStorage.setItem("token", urlData.access_token)
