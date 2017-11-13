e964f99127fc273fbda309e8afbbdb3756b6f287
samdd <sam@samdd.me>
2017-11-13 23:01:38 +0000

    *

--- source/server/server.config.js
+++ source/server/server.config.js
@@ -3,7 +3,8 @@ module.exports = {
     name        : "injectify-server",
     script      : "./main.js",
     node_args: [
-      "--inspect=18999 --web-host 0.0.0.0"
+      "--inspect=18999",
+      " --web-host 0.0.0.0"
     ],
     watch       : false,
     env: {
