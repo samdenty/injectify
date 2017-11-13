88d2cc9be5879ead13b4eaf87b36f5ae24ca382e
samdd <sam@samdd.me>
2017-11-13 23:04:07 +0000

    **

--- source/server/server.config.js
+++ source/server/server.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script      : "./main.js",
     node_args: [
       "--inspect=18999",
-      " --web-host 0.0.0.0"
+      "--web-host=0.0.0.0"
     ],
     watch       : false,
     env: {
