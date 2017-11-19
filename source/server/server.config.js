92852793ad97edb1e1eb64d8e0c508aa27bd428f
Sam Denty <sam@samdd.me>
2017-11-19 17:17:42 +0000

    Fix

--- source/server/server.config.js
+++ source/server/server.config.js
@@ -2,8 +2,10 @@ module.exports = {
   apps : [{
     name        : "injectify-server",
     script      : "./main.js",
-    node_args: [
-      "--inspect-port=0.0.0.0:18999",
+    node_args	: [
+      "--inspect-port=0.0.0.0:18999"
+    ],
+    args	: [
       "--color"
     ],
     watch       : false,
