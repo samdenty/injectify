9e7f6ee129b1cd78c97b1621541a17bf9a479de2
samdd <sam@samdd.me>
2017-11-19 17:33:07 +0000

    [+] Log

--- source/server/server.config.js
+++ source/server/server.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  apps : [{
-    name        : "injectify-server",
-    script      : "./main.js",
-    node_args	: [
-      "--inspect-port=0.0.0.0:18999"
-    ],
-    args	: [
-      "--color"
-    ],
-    watch       : false,
-    env: {
-      "NODE_ENV": "production",
-    }
-  }]
+	apps : [{
+		name            : "injectify-server",
+		script          : "./main.js",
+		log_date_format : "DD/MM/YY  hh:mm",
+		node_args    	  : [
+			"--inspect-port=0.0.0.0:18999"
+		],
+		args           : [
+			"--color"
+		],
+		watch           : false,
+		env							: {
+			"NODE_ENV"		: "production",
+		}
+	}]
 }
