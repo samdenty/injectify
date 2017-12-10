da8cffa263f3863643142a87ded2c22b461ff3e0
samdd <sam@samdd.me>
2017-12-10 01:11:26 +0000

    Added examples

---
+++ source/server/server.config.example.js
@@ -0,0 +1,31 @@
+module.exports = {
+  injectify: {
+    github: {
+      client_id: 'MY_APP_ID',
+      client_secret: 'MY_APP_SECRET'
+    },
+    debug: true,
+    mongodb: 'mongodb://localhost:19000/injectify',
+    express: 3000,
+    dev: process.env.NODE_ENV.toUpperCase() === 'DEVELOPMENT',
+    follow: {
+      enable: false,
+      username: 'samdenty99'
+    }
+  },
+  apps: [{
+    name: 'injectify-server',
+    script: './main.js',
+    log_date_format: 'DD/MM/YY hh:mm',
+    node_args: [
+      '--inspect-port=0.0.0.0:18999'
+    ],
+    args: [
+      '--color'
+    ],
+    watch: false,
+    env: {
+      'NODE_ENV': 'production'
+    }
+  }]
+}
