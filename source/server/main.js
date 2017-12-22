54076f171a49c07a4f38ad5ffa7867efc71ac594
samdd <sam@samdd.me>
2017-12-22 23:20:12 +0000

    *

--- source/server/main.js
+++ source/server/main.js
@@ -77,9 +77,6 @@ injector.loadModules((modules, debugModules, count) => {
 
 console.log(chalk.greenBright('[Injectify] ') + 'listening on port ' + config.express)
 
-setInterval(() => {
-  console.log(injectServer)
-}, 2000)
 process.on('unhandledRejection', (reason, p) => {
   console.log(chalk.redBright('[Promise] ') + ' Unhandled Rejection at:', p, chalk.redBright('\nREASON:'), reason)
 })
