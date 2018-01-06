7799d04ab57c0c3eebb4689747d7a58dd56e02c7
samdd <sam@samdd.me>
2018-01-06 15:01:06 +0000

    :bug: Fixes

--- src/inject/modules/rickroll/module.js
+++ src/inject/modules/rickroll/module.js
@@ -1,7 +1,13 @@
+import { inject } from "./C:/Users/samde/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async";
+
 /**
  * Call the embed module with the YouTube embed
  */
 var autoplay = ''
 if (module.params !== false) autoplay = '?autoplay=true'
-injectify.module('embed', 'https://www.youtube.com/embed/dQw4w9WgXcQ' + autoplay)
+if (injectify.info.platform === 'browser') {
+  injectify.module('embed', 'https://www.youtube.com/embed/dQw4w9WgXcQ' + autoplay)
+} else {
+  injectify.module('embed', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+}
 module.return(true)
