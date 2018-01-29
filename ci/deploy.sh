3beeadcb94e3baf09564d94ee299756a67f83477
samdd <sam@samdd.me>
2018-01-29 15:34:01 +0000

    :black_circle: Deploy fixes

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Tarring build files"
-tar -C ~ -zcvf injectify.tar.gz injectify
+echo -e "\e[35m[deploy.sh]\e[0m \e[93mTarring build files\e[0m"
+tar -C ~ -zcvf ~/injectify.tar.gz injectify
 
-echo "Cleaning remote tar"
+echo -e "\e[35m[deploy.sh]\e[0m \e[93mCleaning remote tar\e[0m"
 ssh injectify@ssh.samdd.me "rm -f injectify.tar.gz"
-echo "Moving tar to server"
-scp injectify.tar.gz injectify@ssh.samdd.me:./
+echo -e "\e[35m[deploy.sh]\e[0m \e[93mUploading tar to server\e[0m"
+scp ~/injectify.tar.gz injectify@ssh.samdd.me:./
 
-echo "Deploying to server"
+echo -e "\e[35m[deploy.sh]\e[0m \e[93mRunning deploy script on server\e[0m"
 ssh injectify@ssh.samdd.me "yarn run deploy:tar"
