0cd0a4838522b8e054b35c95a9637001e1d1c2b8
samdd <sam@samdd.me>
2018-01-29 01:47:01 +0000

    :black_circle: Proper tarballed deployments

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-rm -rf .deliver
-mkdir .deliver
-cp -R www .deliver
-echo 'Deploy.sh'
+echo "Tarring build files"
+tar -C ~ -zcvf injectify.tar.gz injectify
+
+echo "Cleaning remote tar"
+ssh injectify@ssh.samdd.me "rm -f injectify.tar.gz"
+echo "Moving tar to server"
+scp injectify.tar.gz injectify@ssh.samdd.me:./
+
+echo "Deploying to server"
+ssh injectify@ssh.samdd.me "yarn run deploy:tar"
