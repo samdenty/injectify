bb1f17aa6697461af2e2b72ba40c60f47cc9b5af
samdd <sam@samdd.me>
2018-03-25 18:12:52 +0100

    :bug: Fix

--- interface/src/components/Projects/Data/index.jsx
+++ interface/src/components/Projects/Data/index.jsx
@@ -66,8 +66,12 @@ const timeSearch = (value, filter) => {
 }
 
 const timeSort = (a, b) => {
-  if (a.unix === b.unix) return 0
-  return a.unix > b.unix ? -1 : 1
+  if (a.unix && b.unix) {
+    if (a.unix === b.unix) return 0
+    return a.unix > b.unix ? -1 : 1
+  } else {
+    return 0
+  }
 }
 
 const dataSort = (a, b) => {
